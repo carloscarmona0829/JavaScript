d76581d8ccd4f05c495580e52b1b9fb3be123fcf
Carlos Carmona <carloscarmona0829@gmail.com>
2022-06-25 10:30:00 -0500

    ejercicios cuenta regresiva y tema oscuro terminados

---
+++ dom/cuenta_regresiva.js
@@ -0,0 +1,25 @@
+const d = document;
+
+export default function contdown(id, limitDay, finalMessage){
+    const $contdwon = d.getElementById(id),
+    contdownDate = new Date(limitDay).getTime();
+
+    let countdownTempo = setInterval(()=>{
+        let now = new Date().getTime(),
+        limitTime = contdownDate - now,
+        days = Math.floor(limitTime/(1000*60*60*24)),
+        hours = ("0" + Math.floor(limitTime % (1000*60*60*24)/(1000*60*60))).slice(-2),
+        minutes = ("0" + Math.floor(limitTime % (1000*60*60)/(1000*60))).slice(-2),
+        seconds = ("0" + Math.floor(limitTime % (1000*60)/(1000))).slice(-2);
+
+        $contdwon.innerHTML = `<h3>Faltan: ${days} días ${hours} horas ${minutes} minutos y ${seconds} segundos </h3>`;
+
+        if(limitTime < 0){
+            clearInterval(countdownTempo);
+            $contdwon.innerHTML = `<h3> ${finalMessage} </h3>`;    
+        }
+        
+
+        console.log(contdownDate, now, limitTime);
+    },1000)
+}
--- dom/index-dom.js
+++ dom/index-dom.js
@@ -1,6 +1,9 @@
+import scrollTopButton from "./boton-scroll.js";
+import contdown from "./cuenta_regresiva.js";
 import hamburguerMenu from "./menu-_hamburguesa.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall, shortcuts } from "./teclado.js";
+import darkTheme from "./tema_oscuro.js";
 
 const d = document;
 
@@ -8,6 +11,9 @@ d.addEventListener("DOMContentLoaded", e => {
     hamburguerMenu(".panel-btn", ".panel", ".menu a");
     digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
     alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+    contdown("contdown", "Nov 21, 2022 01:00:00","Feliz Cumpleaños Carlos Carmona 😃");
+    scrollTopButton(".scroll-top-btn")
+    darkTheme(".dark-theme-btn", "dark-mode")
 });
 
 d.addEventListener("keydown", (e)=>{    
