c28193fe1d9c7766fae58837a2c342838d26afe9
carloscarmona0829 <carloscarmona0829@gmail.com>
2022-07-30 12:27:41 -0500

    Catálogo de productos consumiento la api stripe

---
+++ ajax-ejercicios/assets/stripe-keys.js
@@ -0,0 +1,4 @@
+ export default {
+    public:"pk_test_51LRGXRDq3swNcIJKGXue2huPY7fzLz0P3ZhC93vJvbPYOyCm0ZzQJv0dGu5eNbiQLKYeMdUGrrCspJgUAoahAbWy00Qwkmakmg",
+    secret:"sk_test_51LRGXRDq3swNcIJKspFzdAW4NeMQdciNzJ9GlHjVY6Nuf2gPm9YZOlIR4dLNg0eQSdh45fhKfYKb0BwTUpJiITsZ00hrgccaqt", 
+ };
