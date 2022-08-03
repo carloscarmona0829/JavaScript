2439396cd71538177a8a7943fa3ef7cadde33dc8
carloscarmona0829 <carloscarmona0829@gmail.com>
2022-08-02 19:35:12 -0500

    pasarela de pagos con stripe, consumo de archivos MarkDown.md, consumo de apis pokemon y tv-shows

--- ajax-ejercicios/assets/stripe-checkout.js
+++ ajax-ejercicios/assets/stripe-checkout.js
@@ -13,6 +13,8 @@ const d = document,
 
 let products, prices;
 
+const moneyFormat = num => `$${num.slice(0, -2)}.${num.slice(-2)}`;
+
 Promise.all([
     fetch("https://api.stripe.com/v1/products", $fetchOptions),
     fetch("https://api.stripe.com/v1/prices", $fetchOptions)
@@ -22,19 +24,19 @@ Promise.all([
         //console.log(json);
         products = json[0].data;
         prices = json[1].data;
-        console.log(products, prices);
+        //console.log(products, prices);
 
         prices.forEach((el) => {
             let productData = products.filter((product) => product.id === el.product);
-            console.log(productData);
+            //console.log(productData);
 
             $template.querySelector(".producto").setAttribute("data-price", el.id);
-            $template.querySelector("img").src=productData[0].images[0];
-            $template.querySelector("img").alt=productData[0].name;
-            $template.querySelector("figcaption").innerHTML=`
+            $template.querySelector("img").src = productData[0].images[0];
+            $template.querySelector("img").alt = productData[0].name;
+            $template.querySelector("figcaption").innerHTML = `
             ${productData[0].name}
             <br>
-            ${el.unit_amount_decimal} ${el.currency}
+            ${moneyFormat(el.unit_amount_decimal)} ${el.currency}
             `;
 
             let $clone = d.importNode($template, true);
@@ -49,3 +51,23 @@ Promise.all([
         $productos.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
     });
 
+d.addEventListener("click", (e) => {
+    console.log(e.target);
+    if(e.target.matches(".producto *")) {
+        let price = e.target.parentElement.getAttribute("data-price");
+        //console.log(price);
+        Stripe(STRIPE_KEYS.public)
+        .redirectToCheckout({
+            lineItems: [{price, quantity: 1}],
+            mode: "subscription",
+            successUrl: "http://127.0.0.1:5500/ajax-ejercicios/assets/stripe-success.html",
+            cancelUrl: "http://127.0.0.1:5500/ajax-ejercicios/assets/stripe-cancel.html",
+        })
+        .then(res=>{
+            if(res.error){
+                console.log(res);
+                $productos.insertAdjacentElement("afterend", res.error.message);
+            }
+        })
+    }
+});
