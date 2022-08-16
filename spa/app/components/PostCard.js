a9b2c6169d7fc3d589d68e092930d38e44af4f58
carloscarmona0829 <carloscarmona0829@gmail.com>
2022-08-16 16:58:10 -0500

    Post, PostCard y Search finalizados

--- spa/app/components/PostCard.js
+++ spa/app/components/PostCard.js
@@ -1,9 +1,16 @@
-export function PostCard(props){
-    let {date, _embedded, slug, title} = props;
+export function PostCard(props) {
+    let { date, id, _embedded, slug, title } = props;
     let dateFormat = new Date(date).toLocaleString(),
-    urlPoster = _embedded["wp:featuredmedia"]
-    ? _embedded["wp:featuredmedia"][0].source_url
-    : "app/assets/Favicon.png";
+        urlPoster = _embedded["wp:featuredmedia"]
+            ? _embedded["wp:featuredmedia"][0].source_url
+            : "app/assets/Favicon.png";
+
+    document.addEventListener("click", (e) => {
+        //Si el click no toca el enlace "".post-card a" entonces retorna y no ejecuta la programación
+        if (!e.target.matches(".post-card a")) return false;
+        //Establece la propiedad "wpPostId", coge el data atribute que es .id. Esto guarda el id del enlace del link seleccionado.
+        localStorage.setItem("wpPostId", e.target.dataset.id);
+    });
 
     return `
     <article class="post-card">
@@ -11,7 +18,7 @@ export function PostCard(props){
     <h2>${title.rendered}</h2>
     <p>
     <time datetime="${date}">${dateFormat}</time>
-    <a href="#/${slug}">Ver Publicación</a>
+    <a href="#/${slug}" data-id="${id}">Ver Publicación</a>
     </p>
     </article>
     `;
--- spa/app/components/Router.js
+++ spa/app/components/Router.js
@@ -1,38 +1,69 @@
 import { ajax } from "../helpers/ajax.js";
 import api from "../helpers/wp_api.js"
+import { Post } from "./Post.js";
 import { PostCard } from "./PostCard.js";
+import { SearchCard } from "./SearchCard.js";
 
-export async function Router(){
+export async function Router() {
 
     let d = document,
-    w = window,
-   $main = d.getElementById("main");
+        w = window,
+        $main = d.getElementById("main");
 
-    let {hash} = location;
-    console.log(hash);
+    let { hash } = location;
+    //console.log(hash);
 
-   $main.innerHTML = null;
+    $main.innerHTML = null;
 
-    if(!hash || hash === "#/"){
+    if (!hash || hash === "#/") {
         await ajax({
             url: api.POSTS,
             cbSuccess: (posts) => {
-                console.log(posts);
-                let html="";
+                //console.log(posts);   
+                let html = "";
                 posts.forEach((post) => (html += PostCard(post)));
                 d.querySelector(".loader").style.display = "none";
-               $main.innerHTML = html;
+                $main.innerHTML = html;
             },
         });
-    }else if (hash.includes("#/search")){
-       $main.innerHTML = "<h2>Sección del Buscador</h2>";
-    }else if (hash=== "#/contacto"){
-       $main.innerHTML = "<h2>Sección Contacto</h2>";
-    }else{
-       $main.innerHTML = "<h2>Contenido del POST Seleccionado</h2>";
-    }   
+
+    } else if (hash.includes("#/search")) {
+        let query = localStorage.getItem("wpSearch");
+
+        if (!query) {
+            d.querySelector(".loader").style.display = "none";
+            return false
+        };
+        await ajax({
+            url: `${api.SEARCH}${query}`,
+            cbSuccess: (search) => {
+                console.log(search);    
+                let html = "";
+                if (search.length === 0) {
+                    html = `
+                    <p class="error">
+                    No existen resultados de búsqueda para el término <mark>${query}</mark>
+                    </p>
+                    `;
+                } else {
+                    search.forEach((post) => (html += SearchCard(post)));
+                }
+                $main.innerHTML = html;
+            },
+        });
+    } else if (hash === "#/contacto") {
+        $main.innerHTML = "<h2>Sección Contacto</h2>";
+    } else {
+        await ajax({
+            url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
+            cbSuccess: (post) => {
+                //console.log(post);
+                $main.innerHTML = Post(post);
+            },
+        });
+    }
 
     d.querySelector(".loader").style.display = "none";
-    
+
 }
 
